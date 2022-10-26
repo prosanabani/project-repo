0888a92e67b381b674c60995b2eda1091323141a
Ali Ibrahim Al-Sanabani <pro.sanabani@gmail.com>
2022-10-26 12:52:32 +0300

    testimonial-part is added

---
+++ index.js
@@ -0,0 +1,13 @@
+let track = document.querySelector('.testimonial-container');
+let slides = Array.from(track.children);
+let nextButton = document.querySelector('.navigation-button#right');
+let prevButton = document.querySelector('.navigation-button#left');
+
+let slideWidth = slides[0].getBoundingClientRect().width;
+
+
+
+
+//arranging slides next to each other
+// when i click left,move the slides to the left ;
+// when i click right,move the slides to the right ; 
