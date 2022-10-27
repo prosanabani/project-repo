c6d180373bf05d5f9c9bfe3a5fdb525d880bde49
Ali Ibrahim Al-Sanabani <pro.sanabani@gmail.com>
2022-10-27 23:34:37 +0300

    simple-changes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-let track = document.querySelector('.testimonial-container');
+let track = document.getElementsByClassName('testimonial-container');
 let slides = Array.from(track.children);
 let nextButton = document.querySelector('.navigation-button#right');
 let prevButton = document.querySelector('.navigation-button#left');
 
 let slideWidth = slides[0].getBoundingClientRect().width;
+
 
 
 
