7e829a6db11df1d8b2cf04de9b8d5b873e0348f8
Ali Ibrahim Al-Sanabani <pro.sanabani@gmail.com>
2022-11-01 01:02:31 +0300

    making the login button move right and left

--- login.js
+++ login.js
@@ -1,5 +1,50 @@
 let userInput = document.getElementById("input-username")
+let cursor_place_lognow = document.getElementById("button-login-div")
+let login_now = document.getElementById("button-login")
 
-let y = userInput.nodeValue;
-console.log(y)
-console.log(userInput)
+
+if (userInput.value == "") {
+    changingState(1)
+}
+else {
+    changingState(0)
+}
+
+
+
+
+
+function changingState(num) {
+    if (num == 1) {
+        var place_x = 0;
+        var place_y = 0;
+        //finding the cursor place
+        function cursorPlace() {
+            cursor_place_lognow.onmousemove = function (e) {
+                let x = e.offsetX;
+                let y = e.offsetY;
+                place_x = x;
+                place_y = y;
+                // console.log(x + " " + y);
+            }
+        }
+        cursorPlace()
+        //moving login right and left
+        cursor_place_lognow.addEventListener('mousemove', function () {
+
+            if (place_x > (cursor_place_lognow.getBoundingClientRect().width / 2)) {
+                login_now.style.transform = "translateX(-150px)"
+            }
+            else {
+                login_now.style.transform = "translateX(150px)"
+            }
+
+        })
+        //reseting the place of the login to default
+        cursor_place_lognow.addEventListener('mouseleave', function () {
+            login_now.style.transform = "translateX(0px)"
+        })
+    }
+    else
+        console.log("nothing to do")
+}
